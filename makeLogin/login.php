db0184ee198aae4acce67fd98df963951b130d54
parkjihong23 <144294724+parkjihong23@users.noreply.github.com>
2023-11-20 23:08:27 +0900

    11월20일 로그인 만들기

---
+++ makeLogin/login.php
@@ -0,0 +1,11 @@
+<?php
+/*if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+*/
+    echo "<h2>Welcome!</h2>";
+    echo "<p>아이디 는: " . $username . "</p>";
+    echo "<p>비밀번호 는: " . $password . "</p>";
+//}
+?>
+
